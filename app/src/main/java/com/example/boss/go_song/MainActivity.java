be40a7280106fc4bea64922c60d79abee07d2f8b
Ardi B <ardy1144malang@gmail.com>
2019-12-03 11:16:29 +0700

    pesan

---
+++ app/src/main/java/com/example/boss/go_song/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.boss.go_song;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Handler handler= new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,login.class);
+                startActivity(intent);
+                finish();
+            }
+        },1000L);
+    }
+}
