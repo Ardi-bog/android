a64ea025afc6c0cf4144ace09c087aa7b822dec4
Ardi B <ardy1144malang@gmail.com>
2019-12-10 11:13:10 +0700

    versi2

---
+++ app/src/main/java/com/example/boss/go_song/profile.java
@@ -0,0 +1,31 @@
+package com.example.boss.go_song;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class profile extends Fragment {
+
+
+    public profile() {
+        // Required empty public constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+}
